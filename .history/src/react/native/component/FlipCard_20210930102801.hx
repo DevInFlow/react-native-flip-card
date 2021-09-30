6cc77fc2d245d0db2559a7a635038c5aaac0ee45
Gábor Váradi <varadi83gabor@gmail.com>
2021-09-30 10:32:51 +0200

    .

---
+++ .history/src/react/native/component/FlipCard_20210930102801.hx
@@ -0,0 +1,7 @@
+package react.native.component;
+imoprt react.native.component.props.FlipCardProps;
+
+@:jsRequire('react-native-flip-card', 'FlipCard')
+extern class FlipCard extends ReactComponentOfProps<FlipCardProps> {}
+    
+}
