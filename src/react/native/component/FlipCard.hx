53f9246b016ef772c28f6fb486d1a5347db01558
Gábor Váradi <varadi83gabor@gmail.com>
2021-09-30 10:28:15 +0200

    add missing import

--- src/react/native/component/FlipCard.hx
+++ src/react/native/component/FlipCard.hx
@@ -1,4 +1,5 @@
 package react.native.component;
+imoprt react.native.component.props.FlipCardProps;
 
 @:jsRequire('react-native-flip-card', 'FlipCard')
 extern class FlipCard extends ReactComponentOfProps<FlipCardProps> {}
